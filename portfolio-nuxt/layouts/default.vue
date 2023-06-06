741171a221a09a31db4100bc338b81ed5d560647
mwinter <mwinter@unboundedpress.org>
2023-06-06 19:51:48 +0200

    slow transition to nuxt

---
+++ portfolio-nuxt/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="text-4xl">michael winter</h1>
+    <h2>
+      <ul class="inline-list">
+        <li>works</li>
+        <li>events</li>
+        <li>about</li>
+        <li>code</li>
+      </ul>
+    </h2>
+    <slot /> <!-- required here only -->
+  </div>
+</template>
+
+<style>
+.inline-list li {
+    display: inline;
+    padding-left: 5px;
+    padding-right: 20px;
+}
+</style>
---
+++ portfolio-nuxt/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Index page</h1>
+  <div class="grid grid-cols-3 gap-4">
+    <ul>
+      <li class="italic" v-for="item in works">{{ item.title }}</li>
+    </ul>
+
+    <ul>
+      <li v-for="item in pubs">{{ item.entryTags.title }}</li>
+    </ul>
+  </div>
+  </template>
+
+<script setup>
+  const { data: works } = await useFetch('https://unboundedpress.org/api/works?sort=-date&pagesize=200')
+  const { data: pubs } = await useFetch('https://unboundedpress.org/api/publications/_aggrs/publications?pagesize=200')
+</script>
